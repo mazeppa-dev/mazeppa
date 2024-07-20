a9b3b03796e7c5d7bfed1acaf908c9732ca43048
hirrolot <hirrolot@gmail.com>
2024-07-20 21:12:57 +0500

    Update the gensym only when needed during postprocessing

--- lib/mechanics/postprocessor.ml
+++ lib/mechanics/postprocessor.ml
@@ -63,7 +63,7 @@ let share_args ~gensym (op, initial_args) =
 let query_env ~env x = Option.value ~default:x (Symbol_map.find_opt x env)
 
 (* TODO: handle other types of restrictions as well. *)
-type restriction = NotEqual of t
+type restriction = NotEqual of Raw_term.t
 
 let handle_term ~(gensym : Gensym.t) ~(env : Renaming.t) t =
     (* We propagate negative information about primitives at residualization-time. During
@@ -76,53 +76,56 @@ let handle_term ~(gensym : Gensym.t) ~(env : Renaming.t) t =
          | _ -> false)
       | _ -> false
     in
+    let hermetic k force =
+        let gensym_backup = Gensym.clone gensym in
+        let result = k force in
+        Gensym.assign ~other:gensym_backup gensym;
+        result
+    in
     let exception Select of Raw_term.t in
     let rec go ~env = function
       | Var x -> Var (query_env ~env x)
       | Const const -> Const const
-      | Call (op, [ Var x; t ]) when op = symbol "=" && is_conflict (x, go_subterm ~env t)
-        -> Call (symbol "F", [])
-      | Call (op, [ t; Var x ]) when op = symbol "=" && is_conflict (x, go_subterm ~env t)
-        -> Call (symbol "F", [])
+      | Call (op, [ Var x; t ])
+        when op = symbol "=" && is_conflict (x, hermetic (go ~env) t) ->
+        Call (symbol "F", [])
+      | Call (op, [ t; Var x ])
+        when op = symbol "=" && is_conflict (x, hermetic (go ~env) t) ->
+        Call (symbol "F", [])
       | Call (op, args) ->
         (* The problem of sharing arguments has optimal substructure: by postprocessing
            arguments before feeding them to [share_args], we make the resulting term share
            all equal arguments which reside on the same syntactical level (a function
            call), throughout all levels in the term. *)
-        share_args ~gensym (op, List.map (go_subterm ~env) args)
+        share_args ~gensym (op, List.map (hermetic (go ~env)) args)
       | Match
           ( (Call (op, ([ Var x; negation ] | [ negation; Var x ])) as t)
           , ([ ((c_f, []), case_f); ((c_t, []), case_t) ] as cases) )
         when op = symbol "=" && c_f = symbol "F" && c_t = symbol "T" ->
         (try
            let t = go_scrutinee ~env ~cases t in
-           let case_f = (c_f, []), go_restrict ~env ~x ~negation case_f in
-           let case_t = (c_t, []), go ~env case_t in
+           let case_f = (c_f, []), hermetic (go_restrict ~env ~x ~negation) case_f in
+           let case_t = (c_t, []), hermetic (go ~env) case_t in
            Raw_term.Match (t, [ case_f; case_t ])
          with
          | Select t -> t)
       | Match (t, cases) ->
         (try
            let t = go_scrutinee ~env ~cases t in
-           let cases = List.map (go_case ~env) cases in
+           let cases = List.map (hermetic (go_case ~env)) cases in
            Raw_term.Match (t, cases)
          with
          | Select t -> t)
       | Let (x, t, u) ->
-        let t = go_subterm ~env t in
+        let t = hermetic (go ~env) t in
         let x' = Gensym.emit gensym in
         let u = go_extend ~env (([ x ], [ x' ]), u) in
         Let (x', t, u)
-    and go_subterm ~env t =
-        let gensym_backup = Gensym.clone gensym in
-        let t = go ~env t in
-        Gensym.assign ~other:gensym_backup gensym;
-        t
     and go_extend ~env ((params, params'), t) =
         let env = Symbol_map.extend2 ~keys:params ~values:params' env in
         go ~env t
     and go_restrict ~env ~x ~negation t =
-        let negation = go_subterm ~env negation in
+        let negation = hermetic (go ~env) negation in
         match negation with
         | Var _ | Const _ ->
           Hashtbl.add restrictions x (NotEqual negation);
@@ -134,15 +137,12 @@ let handle_term ~(gensym : Gensym.t) ~(env : Renaming.t) t =
         let c_params' = Gensym.emit_list ~length_list:c_params gensym in
         (c, c_params'), go_extend ~env ((c_params, c_params'), t)
     and go_scrutinee ~env ~cases t =
-        let gensym_backup = Gensym.clone gensym in
-        let t = go ~env t in
+        let t = hermetic (go ~env) t in
         (match t with
          | Call (c, []) ->
            List.iter
              (function
-               | (c', []), t when c = c' ->
-                 Gensym.assign ~other:gensym_backup gensym;
-                 raise_notrace (Select (go ~env t))
+               | (c', []), t when c = c' -> raise_notrace (Select (go ~env t))
                | _ -> ())
              cases
          | _ -> ());
