b48f9e8318ba7a85bb7fd24ee02b07186326600d
Tima Kinsart <hirrolot@gmail.com>
2024-09-14 17:09:11 -0400

    Refactor `scripts/install.sh`

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+echo "Installing the dependencies..."
 opam install . --deps-only -y
+
+echo "Building Mazeppa..."
 dune build --release
+
+echo "Installing Mazeppa..."
 dune install --release mazeppa
