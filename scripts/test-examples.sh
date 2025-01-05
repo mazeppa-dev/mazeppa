e2890a18eda2ecdc85fd1357379dabb858180365
hirrolot <hirrolot@gmail.com>
2025-01-06 03:27:53 +0500

    Refactor `scripts/test-examples.sh`

--- scripts/test-examples.sh
+++ scripts/test-examples.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-for example in examples/*/; do
-    echo "Testing '$example'..."
-    cd $example
+for example_dir in examples/*/; do
+    echo "Testing '$example_dir'..."
+    cd $example_dir
     ../../scripts/mazeppa.sh run --target-dir test --inspect
     cd ../..
-    diff --recursive "${example}target/" "${example}test/"
-    rm -rf $example/test
+    diff --recursive "${example_dir}target/" "${example_dir}test/"
+    rm -rf $example_dir/test
 done
