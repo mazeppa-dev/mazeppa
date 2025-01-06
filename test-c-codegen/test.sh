6952570b03d2bdc2ed710726f527a2b84491db8e
hirrolot <hirrolot@gmail.com>
2025-01-06 16:35:59 +0500

    Test C codegen with `-Wextra`

--- test-c-codegen/test.sh
+++ test-c-codegen/test.sh
@@ -6,12 +6,11 @@ if [ -z $CC ]; then
     CC=gcc
 fi
 
-common_options="-Wall -Werror -Wno-unused-variable -Wno-infinite-recursion"
-common_options="$common_options -std=gnu11"
+common_options="-std=gnu11 -Wall -Wextra -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-type-limits -Wno-ignored-qualifiers -Wno-infinite-recursion"
 
 # All the examples must at least compile with no errors.
-for example in ../examples/*/; do
-    cat "${example}main.mz" \
+for example_dir in ../examples/*/; do
+    cat "${example_dir}main.mz" \
         | ../scripts/mazeppa.sh translate --language C --entry run \
         | $CC -c -o /dev/null $common_options -xc -
 done
